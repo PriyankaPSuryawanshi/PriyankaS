5ad27d9802f18597139c9775d9aff3ec41eb1389
Priyanka <Priyanka@DESKTOP-NSH22GE>
2022-08-01 17:41:00 +0530

    [Priyanka]: new project

--- src/SAMPLE1/DEMO2.java
+++ src/SAMPLE1/DEMO2.java
@@ -1,5 +1,10 @@
 package SAMPLE1;
 
-public class DEMO2 {
+public class DEMO2 
+{
+	public static void main(String[] args) {
+	
+		System.out.println("Good Afternoon");
+}
 
 }
--- src/SAMPLE1/DEMO3.java
+++ src/SAMPLE1/DEMO3.java
@@ -1,5 +1,10 @@
 package SAMPLE1;
 
-public class DEMO3 {
+public class DEMO3 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Good Night");
+	}
 
 }
